1c1f9dd7cec72b6e7839e0b3fcabdd8af636a4aa
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-12 10:54:17 +0530

    Add links in reviews tab

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,13 @@
 import { dialog } from "../index.js";
 
-const reviewsArray = ["IMDB", "Rotten", "Rotten Tomatoes", "The Verge", "IGN", "The Guardian"];
+const reviewsArray = ["IMDB", "Rotten Tomatoes", "The Verge", "IGN", "The Guardian"];
+const reviewLinks = [
+  "https://www.imdb.com/title/tt3398228/reviews",
+  "https://www.rottentomatoes.com/tv/bojack_horseman",
+  "https://www.theverge.com/2020/2/5/21124097/bojack-horseman-review-series-finale-netflix",
+  "https://www.ign.com/tv/bojack-horseman/reviews",
+  "https://www.theguardian.com/tv-and-radio/2020/jan/31/bojack-horseman-review"
+]
 
 
 function toggleReviewsTab(){
@@ -21,7 +28,11 @@ function toggleReviewsTab(){
     
     let reviewHeading = document.createElement("h2");
     reviewCard.appendChild(reviewHeading);
-    reviewHeading.innerText = reviewsArray[i];
+
+    let reviewLink = document.createElement("a");
+    reviewHeading.appendChild(reviewLink);
+    reviewLink.href = reviewLinks[i];
+    reviewLink.innerText = reviewsArray[i];
   }
 	
 	const closeButton = document.createElement("button");
